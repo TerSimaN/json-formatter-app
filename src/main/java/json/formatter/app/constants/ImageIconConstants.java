302d15b3bea8180f3327e393c8846cd5a4fa4539
TerSimaN <mcsiman998@gmail.com>
2025-07-01 15:40:47 +0300

    Added icon constants

---
+++ src/main/java/json/formatter/app/constants/ImageIconConstants.java
@@ -0,0 +1,14 @@
+package json.formatter.app.constants;
+
+import javax.swing.ImageIcon;
+
+public abstract class ImageIconConstants {
+    public static ImageIcon arrowLeftBoldIcon = new ImageIcon("src/main/resources/icons/arrow-left-bold.png");
+    public static ImageIcon arrowRightBoldIcon = new ImageIcon("src/main/resources/icons/arrow-right-bold.png");
+    public static ImageIcon newFileIcon = new ImageIcon("src/main/resources/icons/new-file.png");
+    public static ImageIcon openFileIcon = new ImageIcon("src/main/resources/icons/open-file.png");
+    public static ImageIcon saveFileIcon = new ImageIcon("src/main/resources/icons/save-file.png");
+    public static ImageIcon copyFileIcon = new ImageIcon("src/main/resources/icons/copy-file.png");
+    public static ImageIcon formatJsonIcon = new ImageIcon("src/main/resources/icons/format-json.png");
+    public static ImageIcon compactJsonIcon = new ImageIcon("src/main/resources/icons/compact-json.png");
+}
