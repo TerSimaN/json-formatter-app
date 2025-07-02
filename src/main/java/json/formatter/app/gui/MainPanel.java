7481b9e79e4c24c1f5f208e3f392bb1d6bbc0912
TerSimaN <mcsiman998@gmail.com>
2025-07-02 22:00:18 +0300

    Refactored code, added createCopyPanel method, added createTransformPanel method, added createDifferencesPanel method

--- src/main/java/json/formatter/app/gui/MainPanel.java
+++ src/main/java/json/formatter/app/gui/MainPanel.java
@@ -21,60 +21,9 @@ public class MainPanel extends JPanel {
         leftJsonEditorPanel = new JsonEditorPanel();
         rightJsonEditorPanel = new JsonEditorPanel();
 
-        // Copy JSON panel
-        JPanel jsonCopyPanel = new JPanel(gridLayout);
-        JLabel copyLabel = new JLabel("Copy");
-        copyLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        jsonCopyPanel.add(copyLabel);
-
-        JPanel btnCopyPanel = new JPanel(customFlowLayout);
-        JButton copyRightToLeftBtn = new JButton(ImageIconConstants.arrowLeftBoldIcon);
-        copyRightToLeftBtn.setToolTipText("Copy the contents of the right panel to the left panel");
-        copyRightToLeftBtn.addActionListener(e -> copyJsonContent(ContentDirection.LEFT));
-        btnCopyPanel.add(copyRightToLeftBtn);
-        JButton copyLeftToRightBtn = new JButton(ImageIconConstants.arrowRightBoldIcon);
-        copyLeftToRightBtn.setToolTipText("Copy the contents of the left panel to the right panel");
-        copyLeftToRightBtn.addActionListener(e -> copyJsonContent(ContentDirection.RIGHT));
-        btnCopyPanel.add(copyLeftToRightBtn);
-        jsonCopyPanel.add(btnCopyPanel);
-
-        // Transform JSON panel
-        JPanel jsonTransformPanel = new JPanel(gridLayout);
-        JLabel transformLabel = new JLabel("Transform");
-        transformLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        jsonTransformPanel.add(transformLabel);
-
-        JPanel btnTransformPanel = new JPanel(customFlowLayout);
-        JButton transformRightToLeftBtn = new JButton(ImageIconConstants.arrowLeftBoldIcon);
-        transformRightToLeftBtn.setToolTipText("Transform the contents of the right panel into the left panel");
-        transformRightToLeftBtn.addActionListener(e -> transformJsonContent(ContentDirection.LEFT));
-        btnTransformPanel.add(transformRightToLeftBtn);
-        JButton transformLeftToRightBtn = new JButton(ImageIconConstants.arrowRightBoldIcon);
-        transformLeftToRightBtn.setToolTipText("Transform the contents of the left panel into the right panel");
-        transformLeftToRightBtn.addActionListener(e -> transformJsonContent(ContentDirection.RIGHT));
-        btnTransformPanel.add(transformLeftToRightBtn);
-        jsonTransformPanel.add(btnTransformPanel);
-
-        // Check JSON differences panel
-        JPanel jsonDifferencesPanel = new JPanel(gridLayout);
-        JLabel differencesLabel = new JLabel("Differences");
-        differencesLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        jsonDifferencesPanel.add(differencesLabel);
-
-        JPanel btnDifferencesPanel = new JPanel(customFlowLayout);
-        JButton compareDifferencesBtn = new JButton("Compare");
-        compareDifferencesBtn.setToolTipText("Highlight the differences between left and right panel contents (currently disabled)");
-        compareDifferencesBtn.addActionListener(e -> compareJsonContent());
-        compareDifferencesBtn.setEnabled(false);
-        btnDifferencesPanel.add(compareDifferencesBtn);
-        JCheckBox enableCompareBtnCheckBox = new JCheckBox();
-        enableCompareBtnCheckBox.addActionListener(e -> {
-            boolean isEnabled = compareDifferencesBtn.isEnabled();
-            compareDifferencesBtn.setToolTipText("Highlight the differences between left and right panel contents (currently enabled)");
-            compareDifferencesBtn.setEnabled(!isEnabled);
-        });
-        btnDifferencesPanel.add(enableCompareBtnCheckBox, 0);
-        jsonDifferencesPanel.add(btnDifferencesPanel);
+        JPanel jsonCopyPanel = createCopyPanel(gridLayout, customFlowLayout);
+        JPanel jsonTransformPanel = createTransformPanel(gridLayout, customFlowLayout);
+        JPanel jsonDifferencesPanel = createDifferencesPanel(gridLayout, customFlowLayout);
 
         // JSON controls panel
         JPanel controlsPanel = new JPanel();
@@ -96,6 +45,97 @@ public class MainPanel extends JPanel {
         this.add(centralPanel);
         this.add(rightJsonEditorPanel);
         this.setBackground(Color.CYAN);
+    }
+
+    /**
+     * Create a copy controls JPanel with the specified layout managers
+     * @param gridLayout the GridLayout to use for the panel
+     * @param flowLayout the FlowLayout to use for the button panel
+     * @return the panel
+     */
+    private JPanel createCopyPanel(LayoutManager gridLayout, LayoutManager flowLayout) {
+        JPanel copyPanel = new JPanel(gridLayout);
+        
+        JLabel copyLabel = new JLabel("Copy");
+        copyLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        copyPanel.add(copyLabel);
+
+        JPanel btnCopyPanel = new JPanel(flowLayout);
+        JButton copyRightToLeftBtn = new JButton(ImageIconConstants.arrowLeftBoldIcon);
+        copyRightToLeftBtn.setToolTipText("Copy the contents of the right panel to the left panel");
+        copyRightToLeftBtn.addActionListener(e -> copyJsonContent(ContentDirection.LEFT));
+        btnCopyPanel.add(copyRightToLeftBtn);
+        
+        JButton copyLeftToRightBtn = new JButton(ImageIconConstants.arrowRightBoldIcon);
+        copyLeftToRightBtn.setToolTipText("Copy the contents of the left panel to the right panel");
+        copyLeftToRightBtn.addActionListener(e -> copyJsonContent(ContentDirection.RIGHT));
+        btnCopyPanel.add(copyLeftToRightBtn);
+        
+        copyPanel.add(btnCopyPanel);
+
+        return copyPanel;
+    }
+
+    /**
+     * Create a transform controls JPanel with the specified layout managers
+     * @param gridLayout the GridLayout to use for the panel
+     * @param flowLayout the FlowLayout to use for the button panel
+     * @return the panel
+     */
+    private JPanel createTransformPanel(LayoutManager gridLayout, LayoutManager flowLayout) {
+        JPanel transformPanel = new JPanel(gridLayout);
+        
+        JLabel transformLabel = new JLabel("Transform");
+        transformLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        transformPanel.add(transformLabel);
+
+        JPanel btnTransformPanel = new JPanel(flowLayout);
+        JButton transformRightToLeftBtn = new JButton(ImageIconConstants.arrowLeftBoldIcon);
+        transformRightToLeftBtn.setToolTipText("Transform the contents of the right panel into the left panel");
+        transformRightToLeftBtn.addActionListener(e -> transformJsonContent(ContentDirection.LEFT));
+        btnTransformPanel.add(transformRightToLeftBtn);
+        
+        JButton transformLeftToRightBtn = new JButton(ImageIconConstants.arrowRightBoldIcon);
+        transformLeftToRightBtn.setToolTipText("Transform the contents of the left panel into the right panel");
+        transformLeftToRightBtn.addActionListener(e -> transformJsonContent(ContentDirection.RIGHT));
+        btnTransformPanel.add(transformLeftToRightBtn);
+        
+        transformPanel.add(btnTransformPanel);
+
+        return transformPanel;
+    }
+
+    /**
+     * Create a differences controls JPanel with the specified layout managers
+     * @param gridLayout the GridLayout to use for the panel
+     * @param flowLayout the FlowLayout to use for the button panel
+     * @return the panel
+     */
+    private JPanel createDifferencesPanel(LayoutManager gridLayout, LayoutManager flowLayout) {
+        JPanel differencesPanel = new JPanel(gridLayout);
+        
+        JLabel differencesLabel = new JLabel("Differences");
+        differencesLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        differencesPanel.add(differencesLabel);
+
+        JPanel btnDifferencesPanel = new JPanel(flowLayout);
+        JButton compareDifferencesBtn = new JButton("Compare");
+        compareDifferencesBtn.setToolTipText("Highlight the differences between left and right panel contents (currently disabled)");
+        compareDifferencesBtn.addActionListener(e -> compareJsonContent());
+        compareDifferencesBtn.setEnabled(false);
+        btnDifferencesPanel.add(compareDifferencesBtn);
+        
+        JCheckBox enableCompareBtnCheckBox = new JCheckBox();
+        enableCompareBtnCheckBox.addActionListener(e -> {
+            boolean isEnabled = compareDifferencesBtn.isEnabled();
+            compareDifferencesBtn.setToolTipText("Highlight the differences between left and right panel contents (currently enabled)");
+            compareDifferencesBtn.setEnabled(!isEnabled);
+        });
+        btnDifferencesPanel.add(enableCompareBtnCheckBox, 0);
+
+        differencesPanel.add(btnDifferencesPanel);
+
+        return differencesPanel;
     }
 
     private void copyJsonContent(ContentDirection direction) {
