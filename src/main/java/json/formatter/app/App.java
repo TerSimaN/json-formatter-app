143a61fcc1e7d2bac1b8a2cb17b938067f17afa4
TerSimaN <mcsiman998@gmail.com>
2025-07-04 15:29:15 +0300

    Updated code

--- src/main/java/json/formatter/app/App.java
+++ src/main/java/json/formatter/app/App.java
@@ -1,10 +1,20 @@
 package json.formatter.app;
 
+import javax.swing.SwingUtilities;
+
 import json.formatter.app.gui.MainWindowFrame;
 
 public class App {
     public static void main(String[] args) {
-        MainWindowFrame mainWindowFrame = new MainWindowFrame();
-        mainWindowFrame.createAndShowGUI();
+        SwingUtilities.invokeLater(new Runnable() {
+            MainWindowFrame mainWindowFrame = new MainWindowFrame();
+            
+            @Override
+            public void run() {
+                mainWindowFrame.createAndShowGUI();
+            }
+        });
+
+        // System.out.println("isEventDispatchThread: " + SwingUtilities.isEventDispatchThread());
     }
 }
