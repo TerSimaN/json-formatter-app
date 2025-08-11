4f15c25a6f93aa1d65a5f30843f1fa06c6b33537
TerSimaN <mcsiman998@gmail.com>
2025-08-11 15:31:28 +0300

    Changed icon paths

--- src/main/java/json/formatter/app/constants/ImageIconConstants.java
+++ src/main/java/json/formatter/app/constants/ImageIconConstants.java
@@ -3,14 +3,14 @@ package json.formatter.app.constants;
 import javax.swing.ImageIcon;
 
 public abstract class ImageIconConstants {
-    public static ImageIcon arrowLeftBoldIcon = new ImageIcon("src/main/resources/icons/arrow-left-bold.png");
-    public static ImageIcon arrowRightBoldIcon = new ImageIcon("src/main/resources/icons/arrow-right-bold.png");
-    public static ImageIcon newFileIcon = new ImageIcon("src/main/resources/icons/new-file.png");
-    public static ImageIcon openFileIcon = new ImageIcon("src/main/resources/icons/open-file.png");
-    public static ImageIcon saveFileIcon = new ImageIcon("src/main/resources/icons/save-file.png");
-    public static ImageIcon copyFileIcon = new ImageIcon("src/main/resources/icons/copy-file.png");
-    public static ImageIcon formatJsonIcon = new ImageIcon("src/main/resources/icons/format-json.png");
-    public static ImageIcon compactJsonIcon = new ImageIcon("src/main/resources/icons/compact-json.png");
-    public static ImageIcon wrapEnableIcon = new ImageIcon("src/main/resources/icons/wrap-enabled.png");
-    public static ImageIcon wrapDisableIcon = new ImageIcon("src/main/resources/icons/wrap-disabled.png");
+    public static ImageIcon arrowLeftBoldIcon = new ImageIcon("src/main/resources/json/formatter/app/icons/arrow-left-bold.png");
+    public static ImageIcon arrowRightBoldIcon = new ImageIcon("src/main/resources/json/formatter/app/icons/arrow-right-bold.png");
+    public static ImageIcon newFileIcon = new ImageIcon("src/main/resources/json/formatter/app/icons/new-file.png");
+    public static ImageIcon openFileIcon = new ImageIcon("src/main/resources/json/formatter/app/icons/open-file.png");
+    public static ImageIcon saveFileIcon = new ImageIcon("src/main/resources/json/formatter/app/icons/save-file.png");
+    public static ImageIcon copyFileIcon = new ImageIcon("src/main/resources/json/formatter/app/icons/copy-file.png");
+    public static ImageIcon formatJsonIcon = new ImageIcon("src/main/resources/json/formatter/app/icons/format-json.png");
+    public static ImageIcon compactJsonIcon = new ImageIcon("src/main/resources/json/formatter/app/icons/compact-json.png");
+    public static ImageIcon wrapEnableIcon = new ImageIcon("src/main/resources/json/formatter/app/icons/wrap-enabled.png");
+    public static ImageIcon wrapDisableIcon = new ImageIcon("src/main/resources/json/formatter/app/icons/wrap-disabled.png");
 }
